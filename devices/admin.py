8303e2820c4a84719e1350578b0a3bbe9b57b6c0
kamala96 <jovinjohn959@gmail.com>
2023-07-19 18:41:14 +0300

    added models

--- devices/admin.py
+++ devices/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from .models import Proximity
+from .models import DeviceType, Controller, Sensor, Proximity
 
 # Register your models here.
 
 
+@admin.register(DeviceType)
+class DeviceTypeAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
+
+
+@admin.register(Controller)
+class ControllerAdmin(admin.ModelAdmin):
+    list_display = ("type", "model", "network_address", "unique_id")
+
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ("model", "type", "controller", "unique_id")
+
+
 @admin.register(Proximity)
 class ProximityAdmin(admin.ModelAdmin):
-    list_display = ("distance", "timestamp")
+    list_display = ("sensor", "distance", "timestamp")
--- devices/models.py
+++ devices/models.py
@@ -1,25 +1,61 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
 
-class DeviceType:
-    title = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    description = models.TextField(max_length=200)
-
-
-class Controllers:
-    address = models.CharField(max_length=100, unique=True)
+class DeviceType(models.Model):
     title = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Device: Types"
+        verbose_name_plural = "Device: Types"
+
+
+class Controller(models.Model):
+    type = models.ForeignKey(
+        DeviceType, on_delete=models.CASCADE, related_name='type_controllers')
+    # owner = models.ForeignKey(
+    #     DeviceType, on_delete=models.CASCADE, related_name='controllers')
+    model = models.CharField(max_length=100)
+    network_address = models.GenericIPAddressField()
+    unique_id = models.CharField(max_length=100, unique=True)
+    access_key = models.CharField(max_length=128, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.type} {self.model} - {self.unique_id}'
+
+    def save(self, *args, **kwargs):
+        if self._state.adding or 'access_key' in self.get_deferred_fields():
+            self.access_key = make_password(self.access_key)
+        super(Controller, self).save(*args, **kwargs)
+
+
+class Sensor(models.Model):
+    type = models.ForeignKey(
+        DeviceType, on_delete=models.CASCADE, related_name='type_sensors')
+    model = models.CharField(max_length=100)
+    unique_id = models.CharField(max_length=100, unique=True)
+    controller = models.ForeignKey(
+        Controller, on_delete=models.CASCADE, related_name='controller_sensors')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.type} {self.model} - {self.unique_id}'
 
 
 class Proximity(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='sensor_proximity', null=True)
     distance = models.DecimalField(max_digits=5, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.distance} at {self.timestamp}'
+        return f'{self.sensor}: {self.distance} at {self.timestamp}'
 
     class Meta:
         ordering = ("timestamp", )
